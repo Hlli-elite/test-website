58ed35f1012c793e740a05c48aac05160ee2a55b
Hlli-elite <hllielitect@gmail.com>
2026-05-05 15:23:48 -0400

    Add towers and crosspaths data

---
+++ js/data/towers.js
@@ -0,0 +1,31 @@
+const TOWERS = [
+  ['DartMonkey','Dart Monkey'],['BoomerangMonkey','Boomerang Monkey'],
+  ['BombShooter','Bomb Shooter'],['TackShooter','Tack Shooter'],
+  ['IceMonkey','Ice Monkey'],['GlueGunner','Glue Gunner'],
+  ['Desperado','Desperado'],['SniperMonkey','Sniper Monkey'],
+  ['MonkeySub','Monkey Sub'],['MonkeyBuccaneer','Monkey Buccaneer'],
+  ['MonkeyAce','Monkey Ace'],['HeliPilot','Heli Pilot'],
+  ['MortarMonkey','Mortar Monkey'],['DartlingGunner','Dartling Gunner'],
+  ['WizardMonkey','Wizard Monkey'],['SuperMonkey','Super Monkey'],
+  ['NinjaMonkey','Ninja Monkey'],['Alchemist','Alchemist'],
+  ['Druid','Druid'],['Mermonkey','Mermonkey'],
+  ['BananaFarm','Banana Farm'],['SpikeFactory','Spike Factory'],
+  ['MonkeyVillage','Monkey Village'],['EngineerMonkey','Engineer Monkey'],
+  ['BeastHandler','Beast Handler'],
+];
+
+const CROSSPATHS = [
+  '5-2-0','5-0-2','5-1-0','5-0-1','5-0-0',
+  '2-5-0','0-5-2','1-5-0','0-5-1','0-5-0',
+  '2-0-5','0-2-5','1-0-5','0-1-5','0-0-5',
+  '4-2-0','4-0-2','4-1-0','4-0-1','4-0-0',
+  '2-4-0','0-4-2','1-4-0','0-4-1','0-4-0',
+  '2-0-4','0-2-4','1-0-4','0-1-4','0-0-4',
+  '3-2-0','3-0-2','3-1-0','3-0-1','3-0-0',
+  '2-3-0','0-3-2','1-3-0','0-3-1','0-3-0',
+  '2-0-3','0-2-3','1-0-3','0-1-3','0-0-3',
+  '2-2-0','2-0-2','2-1-0','2-0-1','2-0-0',
+  '0-2-2','1-2-0','0-2-1','0-2-0','1-0-2',
+  '0-1-2','0-0-2','1-1-0','1-0-1','1-0-0',
+  '0-1-1','0-1-0','0-0-1','0-0-0',
+];
